5e67b2695a6d0356f796e0ea1f2a74fb1074849f
VacuumPz <92035639+VacuumPz@users.noreply.github.com>
2021-10-16 04:47:18 +0300

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,144 @@
+/**
+ * Java 1. Homework 3
+ *
+ * @author Gleb
+ * @version 14.10.2021
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+class TicTacToe {
+	final char SIGN_X = 'x';
+	final char SIGN_O = 'o';
+	final char SIGN_EMPTY = '.';
+	char[][] table;
+	Random random;
+	Scanner scanner;
+	
+	public static void main(String[] args) {
+		new TicTacToe().game();
+	}
+	
+	TicTacToe() {
+		table = new char[3][3];
+		random = new Random();
+		scanner = new Scanner(System.in);
+	}
+	
+	void game() {
+		initTable();
+		while (true) {
+			printTable();
+			turnHuman();
+			if (isWin(SIGN_X)) {
+				System.out.println("YOU WIN!");
+				break;
+			}
+			if (isTableFull()) {
+				System.out.println("Sorry, DRAW...");
+				break;
+			}
+			turnAI();
+			if (isWin(SIGN_O)) {
+				System.out.println("AI WIN!");
+				break;
+			}
+			if (isTableFull()) {
+				System.out.println("Sorry, DRAW...");
+				break;
+			}
+		}
+		printTable();
+	}
+	
+	void initTable() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				table[i][j] = SIGN_EMPTY;
+			}
+		}
+	}
+	
+	void printTable() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(table[j][i] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	void turnHuman() {
+		int x, y;
+		do {
+			System.out.print("Enter [1..3] x y: ");
+			x = scanner.nextInt() - 1;
+			y = scanner.nextInt() - 1;
+		} while (!isCellValid(x, y));
+		table[x][y] = SIGN_X;
+	}
+	
+	void turnAI() {
+		int x, y;
+		do {
+			x = random.nextInt(3);
+			y = random.nextInt(3);
+		} while (!isCellValid(x, y));
+		table [x][y] = SIGN_O;
+	}
+	
+	boolean isCellValid(int x, int y) {
+		if (x < 0 || x > 2 || y < 0 || y > 2) {
+			return false;
+		}
+		return table[x][y] == SIGN_EMPTY;
+	}
+	
+	boolean isWin(char ch) {
+/*		if (table[0][0] == ch && table[1][0] == ch && table[2][0] == ch) return true;
+		if (table[0][1] == ch && table[1][1] == ch && table[2][1] == ch) return true;
+		if (table[0][2] == ch && table[1][2] == ch && table[2][2] == ch) return true;
+
+		if (table[0][0] == ch && table[1][1] == ch && table[0][2] == ch) return true;
+		if (table[1][0] == ch && table[1][1] == ch && table[1][2] == ch) return true;
+		if (table[2][0] == ch && table[2][1] == ch && table[2][2] == ch) return true;
+		
+		if (table[0][0] == ch && table[1][1] == ch && table[2][2] == ch) return true;
+		if (table[2][0] == ch && table[1][1] == ch && table[0][2] == ch) return true;
+*/
+/*		boolean diagLeft, diagRight, cols, rows;
+		diagLeft = true;
+		diagRight = true;
+		for (int i = 0; i < 3; i++) {
+			diagLeft &= (table[i][i] == ch);
+			diagRight &= (table[3 - i - 1][i] == ch);
+		}
+		for (int col = 0; col < 3; col++) {
+			cols = true;
+			rows = true;
+			for (int row = 0; row < 3; row++) {
+				cols &= (table[col][row] == ch);
+				rows &= (table[row][col] == ch);
+			}
+			if (cols || rows) return true;
+		}
+		if (diagLeft || diagRight) return true;
+*/		
+		for (int i = 0; i < 3; i++)
+			if ((table[i][0] == ch && table[i][1] == ch && table[i][2] == ch) || (table[0][i] == ch && table[1][i] == ch && table[2][i] == ch)) return true;
+			if ((table[0][0] == ch && table[1][1] == ch && table[2][2] == ch) || (table[2][0] == ch && table[1][1] == ch && table[0][2] == ch)) return true;
+		return false;
+	}
+	
+	boolean isTableFull() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (table[i][j] == SIGN_EMPTY) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
